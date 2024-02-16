75e399bb3a9d803bf261b6b11c4152d7ebd1e38f
mael <mpineau@cerfos.net>
2024-02-16 09:41:26 +0100

    feat: Fetch the API to retrieve a list of weather.

---
+++ app/src/main/java/com/example/b3_android/DTO/WeatherType.java
@@ -0,0 +1,22 @@
+package com.example.b3_android.DTO;
+
+public class WeatherType {
+    private int id;
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
